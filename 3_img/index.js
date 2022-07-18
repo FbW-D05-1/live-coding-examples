36da2cc0547da2b435444c5071dd9743658e5a13
Ari Razab <arianrazab@yahoo.com>
2022-07-18 09:30:29 +0200

    js random array

---
+++ 3_img/index.js
@@ -0,0 +1,5 @@
+const bozos = ["Hosam", "Thomas", "Michael", "Ahmad", "Evgeniia", "Derya", "Klaus", "Lina", "AliBozo", "Etienne", "Mirna", "AhmadJ", "Michelle", "AliR", "Marcel", "Kathrine", "Christopher", "Edgar", "Lea", "Youssef", "Niko", "George", "Marvin"];
+
+let result = bozos[Math.floor(Math.random()*bozos.length)];
+
+console.log(result)
