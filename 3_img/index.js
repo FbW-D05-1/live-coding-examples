fece6dd3d1b9b505e83317732a9adba158c39a16
Ari Razab <arianrazab@yahoo.com>
2022-07-19 15:24:51 +0200

    nth type and child

--- 3_img/index.js
+++ 3_img/index.js
@@ -1,4 +1,4 @@
-const bozos = ["Hosam", "Thomas", "Michael", "Ahmad", "Evgeniia", "Derya", "Klaus", "Lina", "AliBozo", "Etienne", "Mirna", "AhmadJ", "Michelle", "AliR", "Marcel", "Kathrine", "Christopher", "Edgar", "Lea", "Youssef", "Niko", "George", "Marvin"];
+const bozos = ["Hosam", "Michael", "Ahmad", "Evgeniia", "Derya", "Klaus", "Lina", "AliBozo", "Etienne", "Mirna", "AhmadJ", "Michelle", "AliR", "Edgar", "Lea", "Niko", "George", "Marvin"];
 
 let result = bozos[Math.floor(Math.random()*bozos.length)];
 
