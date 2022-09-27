1e777145c789ac36bc826c872e060c336c5e9d09
Ari Razab <arianrazab@yahoo.com>
2022-09-27 13:50:54 +0200

    mathexp

---
+++ 2_Programming-Basics/lc1/js/04_mathematicalExp.js
@@ -0,0 +1,75 @@
+// Mathematical expressions:
+// +, -, *, /, modulo- %, exponentiation- **
+
+console.log(1 + 1); //2
+console.log("1 + 100"); // 1 + 100
+console.log(1 + 100); //101
+
+// concatenation
+console.log("Hello", "World");
+console.log("2" + " " + "3");
+console.log("Good morning" + "Class" + 32);
+
+// console.log("350: func timeout", Function());
+
+// modulo: %
+// returns the remainder
+console.log(10 % 3);
+
+// Operand: is what operators are applied to.
+
+// 5 * 2 : 2 operands 5 and 2.
+
+// operator: +
+// operands: 5 and 8
+// operation: binary because two operands are involved
+
+// exponentiation- **
+
+// String concatenation with binary
+
+let strOne = "Hello";
+let strTwo = "World";
+
+console.log(strOne + " " + strTwo); // Hello World
+
+console.log("2" + 1); // "21"
+console.log(1 + "2"); //"12"
+console.log(2 + 2 + "1"); //"41"
+console.log("2" + 2 + 1); //"221"
+console.log(2 + 1 + "2"); //"32"
+console.log("2" + (2 + 1)); //"23"
+
+let bababooey = "2" + (2 + 1);
+
+console.log(typeof bababooey);
+
+// BODMAS: 2 + (1 * 5) / 4
+// Bracket
+// Open
+// Divide
+// Multiply
+// Add
+// Subtract
+// ----------X---------
+
+// Declaration
+
+// Reassignment
+
+// increment ++
+// decrement --
+
+let counter = 2;
+// counter = 6;
+console.log(counter++);
+// 2 + 1
+console.log(counter);
+// 3 + 1
+console.log(++counter);
+
+let counter2 = 2;
+
+console.log(counter2--);
+console.log(counter2);
+console.log(--counter2);
---
+++ 2_Programming-Basics/lc1/js/Exercise.md
@@ -0,0 +1,21 @@
+# EXERCISE 1
+## Write the code to execute a SUM between the number 12 and 20
+
+
+# EXERCISE 2
+## Create a variable named X containing the number 12
+
+# EXERCISE 3
+## Create a variable named name containing the string John Doe
+
+
+# EXERCISE 4
+## Execute a DIFFERENCE between the number 12 and the variable X, which stores the value 12
+
+# EXERCISE 5
+## Concatinate string with any variables you have written above
+
+# EXERCISE 6
+## Create different variables and check their type
+
+
