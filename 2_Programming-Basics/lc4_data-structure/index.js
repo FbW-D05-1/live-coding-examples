28c8cd9092b84e24eddcf6a80c688f81d73227d7
Ari Razab <arianrazab@yahoo.com>
2022-10-20 16:50:33 +0200

    typescript compiling let consts and arrow functions

--- 2_Programming-Basics/lc4_data-structure/index.js
+++ 2_Programming-Basics/lc4_data-structure/index.js
@@ -1,47 +1,50 @@
 console.log("Hello typescript");
 console.log(typeof NaN);
 // compile => tsc 'nameoffile'.ts
-var x = 3.3;
-var y = "random";
-var hosam = true;
-var array;
+// let 'variable name' : 'type' = 'value'
+let x = 3.3;
+let y = "random";
+const hosam = true;
+let array;
 console.log(array);
-var a;
-var addition = function (n1, n2) {
-  if (n2 === void 0) {
-    n2 = "12";
-  }
-  if (typeof n1 === "number" && typeof n2 === "number") {
-    return n1 + n2;
-  } else {
-    console.log("Error: not a number");
-  }
+let a;
+const addition = (n1, n2 = "12") => {
+    if (typeof n1 === 'number' && typeof n2 === 'number') {
+        return n1 + n2;
+    }
+    else {
+        console.log("Error: not a number");
+    }
 };
 console.log(addition(2, "3"));
-var obj = {
-  itemId: 123,
-  shape: "round",
-  color: "red",
-  size: true,
+// const 'object name' : 'name of our interface' 
+//if one of the keys and typings don't fit here there will be instantly and error
+const obj = {
+    itemId: 123,
+    shape: "round",
+    color: "red",
+    size: true
 };
-var DciStuds = [
-  {
-    name: "Kathrine",
-    age: 24,
-    role: "Student",
-    isTeacher: false,
-  },
-  {
-    name: "Ali",
-    age: 24,
-    role: "Student",
-    isTeacher: false,
-  },
-  {
-    name: "Sidar",
-    age: 24,
-    role: "Student",
-    isTeacher: true,
-  },
+//creating an array with objects and using an interface for the nested objects
+let DciStuds = [
+    {
+        name: "Kathrine",
+        age: 24,
+        role: "Student",
+        isTeacher: false,
+    },
+    {
+        name: "Ali",
+        age: 24,
+        role: "Student",
+        isTeacher: false,
+    },
+    {
+        name: "Sidar",
+        age: 24,
+        role: "Student",
+        isTeacher: true,
+    },
 ];
 console.log(DciStuds[2].name);
+//# sourceMappingURL=index.js.map
