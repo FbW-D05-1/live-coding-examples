4ccebb8eed2a44cd82c9392e84c6e3496cb8c587
Ari Razab <arianrazab@yahoo.com>
2022-10-26 11:45:52 +0200

    reduce comments

--- 2_Programming-Basics/lc4_data-structure/11-reduce.js
+++ 2_Programming-Basics/lc4_data-structure/11-reduce.js
@@ -1,3 +1,22 @@
+// https://javascript.info/array-methods
+
+// The reduce() method executes a reducer function (that you provide) on each member of the array resulting in a single output value.
+
+// When we need to iterate over an array – we can use forEach, for or for..of.
+
+// When we need to iterate and return the data for each element – we can use map.
+
+// The methods 'arr.reduce' and 'arr.reduceRight' are used to calculate a single value based on the array.
+
+// The syntax is:
+
+// const value = arr.reduce(function(previousValue, item, index, array) {
+//   // ...
+// }, initial);
+
+// Your reducer function's returned value is assigned to the accumulator,
+// whose value is remembered across each iteration throughout the array and ultimately becomes the final, single resulting value.
+
 const numbers = [1, 2, 3, 4];
 
 const sumReducer = (acc, cur) => acc + cur;
